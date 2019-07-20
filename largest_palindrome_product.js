3fa352607efecd4f691ac073de7beb69cf612f63
Keith <kayzee@live.com>
2019-07-20 09:50:22 -1000

    2 passing test

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -5,18 +5,36 @@
  *  @return {Object} an object containing the two factors used to produce
  *                   the palindromeNumber and the palindromeNumber itself.
  */
-module.exports = function(digits){
+module.exports = function(digits) {
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
+  let init = Math.pow(10, digits) - 1;
 
-  // do your work here
+  function isPalindrome(num) {
+    let numToString = num.toString();
+    const reverse = numToString
+      .split("")
+      .reverse()
+      .join("");
 
+    return numToString === reverse;
+  }
 
+  for (let i = init; i >= 0; i--) {
+    for (let j = init; j >= 0; j--) {
+      let value = i * j;
+      if (isPalindrome(value) && palindromeNumber < value) {
+        palindromeNumber = value;
+        factor_0 = i;
+        factor_1 = j;
+      }
+    }
+  }
 
   return {
-    factor_0 : factor_0,
-    factor_1 : factor_1,
-    palindromeNumber : palindromeNumber
+    factor_0: factor_0,
+    factor_1: factor_1,
+    palindromeNumber: palindromeNumber
   };
 };
